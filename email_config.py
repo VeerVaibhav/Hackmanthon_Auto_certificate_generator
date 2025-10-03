bc4b400f1016b5a1aaa684dce254c8ff6e827f1f
Vaibhav Raj Singh <vaibhav.raj2025@lpu.in>
2025-10-04 05:21:46 +0530

    Update email_config.py

--- email_config.py
+++ email_config.py
@@ -1,2 +1,3 @@
 EMAIL_ADDRESS = "v.r.always.winner@gmail.com"
-SENDGRID_API_KEY = "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"
+SENDGRID_API_KEY = "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"
+

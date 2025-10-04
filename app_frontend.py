1beff93b94eb7c9c5a24274feb2215b0ddc6c618
Vaibhav Raj Singh <vaibhav.raj2025@lpu.in>
2025-10-04 12:33:13 +0530

    debugging app_frontend.py

--- app_frontend.py
+++ app_frontend.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
 from app_core import read_csv, show_certificates, send_all_emails
-import streamlit as st
 
-EMAIL_ADDRESS = st.secrets["EMAIL_ADDRESS"]
-SENDGRID_API_KEY = st.secrets["SENDGRID_API_KEY"]
+from app_core import EMAIL_ADDRESS, SENDGRID_API_KEY
+
+st.write("Loaded email:", EMAIL_ADDRESS)
+st.write("API key starts with:", SENDGRID_API_KEY[:6])
+
 
 
 st.set_page_config(page_title="Certificate Automation", page_icon="🎓", layout="wide")
